153f47f7b3cf973263fc0f242cd6249b022678c5
spaenleh <spaenleh@gmail.com>
2025-01-08 13:38:41 +0100

    chore: update comment with better phrasing

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -1,8 +1,11 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
-// legacy redirection for routes using the old `signin` route.
-// this should be removed 6 months after the push of the new client interface in production.
-// estimated date at time of writing is July 2025
+/**
+ * Legacy redirection for routes using the old `signin` route.
+ *
+ * This should be removed 6 months after the push of the new client interface in production,
+ * approximately around July 2025.
+ */
 export const Route = createFileRoute('/signin')({
   beforeLoad: (ctx) => {
     throw redirect({ to: '/auth/login', search: ctx.search });
