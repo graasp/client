339c34f62eb6833341a6f610e86e58da44a773e8
spaenleh <spaenleh@gmail.com>
2025-01-10 12:45:29 +0100

    fix: remove start entrypoints

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
 
-import { CssBaseline } from '@mui/material';
+import {
+  CssBaseline,
+  Direction,
+  ThemeProvider as MuiThemeProvider,
+} from '@mui/material';
 
 import { BUILDER_ITEMS_PREFIX, ClientHostManager, Context } from '@graasp/sdk';
+import rtlPlugin from '@graasp/stylis-plugin-rtl';
+import { theme } from '@graasp/ui';
 
+import createCache from '@emotion/cache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import {
   init as SentryInit,
   browserTracingIntegration,
   replayIntegration,
 } from '@sentry/react';
-import { RouterProvider } from '@tanstack/react-router';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { prefixer } from 'stylis';
 
 import '@/config/i18n';
 
@@ -25,6 +36,7 @@ import {
   SENTRY_ENV,
 } from './config/env';
 import { QueryClientProvider, queryClient } from './config/queryClient';
+import { routeTree } from './routeTree.gen';
 
 SentryInit({
   dsn: SENTRY_DSN,
@@ -52,23 +64,80 @@ ClientHostManager.getInstance()
   .addPrefix(Context.Builder, BUILDER_ITEMS_PREFIX)
   .addHost(Context.Builder, new URL(GRAASP_BUILDER_HOST));
 
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  context: {
+    // at this stage, we set it to `undefined`. A more appropriate value will be set later in AuthProvider when we wrap the app.
+    auth: undefined!,
+  },
+});
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function InnerApp() {
   const auth = useAuth();
 
   return <RouterProvider router={router} context={{ auth }} />;
 }
 
+type ThemeWrapperProps = {
+  children: ReactNode;
+};
+
+const getCacheForDirection = (direction?: Direction): EmotionCache =>
+  createCache({
+    key: `mui-dir-${direction}`,
+    stylisPlugins: [prefixer, ...(direction === 'rtl' ? [rtlPlugin] : [])],
+  });
+
+function ThemeWrapper({ children }: Readonly<ThemeWrapperProps>): JSX.Element {
+  // use the hook as it allows to use the correct instance of i18n
+  const { i18n: i18nInstance } = useTranslation();
+  const direction = i18nInstance.dir(i18nInstance.language);
+
+  // needed to set the right attribute on the HTML
+  useEffect(
+    () => {
+      const dir = i18nInstance.dir(i18nInstance.language);
+      console.debug(
+        `Language changed to ${i18nInstance.language}, updating direction to ${dir}`,
+      );
+      document.documentElement.setAttribute('dir', dir);
+    },
+    // here we need to react to the change of the language, the instance does not change.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [i18nInstance.language],
+  );
+
+  return (
+    <MuiThemeProvider theme={{ ...theme, direction }}>
+      <CacheProvider value={getCacheForDirection(direction)}>
+        {children}
+      </CacheProvider>
+    </MuiThemeProvider>
+  );
+}
+
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeWrapper>
-        <CssBaseline />
-        <AuthProvider>
-          <ToastContainer stacked position="bottom-left" />
-          <InnerApp />
-        </AuthProvider>
-      </ThemeWrapper>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeWrapper>
+          <CssBaseline />
+          <AuthProvider>
+            <ToastContainer stacked position="bottom-left" />
+            <InnerApp />
+          </AuthProvider>
+        </ThemeWrapper>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
