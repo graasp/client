a9accf89fc88a106b8215a8dacb29c4949bfe504
spaenleh <spaenleh@gmail.com>
2025-01-10 12:50:37 +0100

    fix: small changes

--- src/modules/analytics/common/ChartTitle.tsx
+++ src/modules/analytics/common/ChartTitle.tsx
@@ -1,5 +1,8 @@
-import { Info as InfoIcon } from '@mui/icons-material';
 import { Stack, Tooltip, Typography } from '@mui/material';
+
+import { useButtonColor } from '@graasp/ui';
+
+import { Info } from 'lucide-react';
 
 const ChartTitle = ({
   title,
@@ -7,21 +10,24 @@ const ChartTitle = ({
 }: {
   title: string;
   description?: string;
-}): JSX.Element => (
-  <Stack
-    spacing={1}
-    pt={2}
-    direction="row"
-    alignItems="center"
-    justifyContent="center"
-  >
-    <Typography variant="h6" align="center">
-      {title}
-    </Typography>
-    <Tooltip title={description}>
-      <InfoIcon color="primary" />
-    </Tooltip>
-  </Stack>
-);
+}): JSX.Element => {
+  const { color } = useButtonColor('primary');
+  return (
+    <Stack
+      spacing={1}
+      pt={2}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant="h6" align="center">
+        {title}
+      </Typography>
+      <Tooltip title={description}>
+        <Info color={color} />
+      </Tooltip>
+    </Stack>
+  );
+};
 
 export default ChartTitle;
