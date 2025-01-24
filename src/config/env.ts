2515cf3390d54ff9f042131a89205ff94e443772
spaenleh <spaenleh@gmail.com>
2025-01-24 10:15:17 +0100

    fix: h5p integration and require h5p env var in prod build

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,8 +15,16 @@ export const GRAASP_REDIRECTION_HOST = import.meta.env
   .VITE_GRAASP_REDIRECTION_HOST;
 export const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
+// expect integration url to be present in production
+// we should not use the backend integration in production as it exposes the cookies to the h5p files if they are served on the same domain.
+if (import.meta.env.PROD && !import.meta.env.VITE_GRAASP_H5P_INTEGRATION_URL) {
+  throw new Error(
+    "Missing required env variable in production: 'VITE_GRAASP_H5P_INTEGRATION_URL'",
+  );
+}
+
 export const H5P_INTEGRATION_URL =
-  import.meta.env.VITE_GRAASP_H5P_INTEGRATION_URL ||
+  import.meta.env.VITE_GRAASP_H5P_INTEGRATION_URL ??
   `${API_HOST}/items/h5p-assets/integration.html`;
 
 // Question: should we host the pdf player assets inside the public directory here instead of at another bucket ?
