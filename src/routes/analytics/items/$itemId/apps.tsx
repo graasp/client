a9f19cbfcee39ea8c4d0b77118f2f6406e5b5379
spaenleh <spaenleh@gmail.com>
2025-01-30 07:34:27 +0100

    fix: width of container grid for apps in analytics

--- src/routes/analytics/items/$itemId/apps.tsx
+++ src/routes/analytics/items/$itemId/apps.tsx
@@ -1,16 +1,25 @@
 import { type JSX, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Grid2 } from '@mui/material';
+import { Box, Grid2, Typography } from '@mui/material';
+
+import { Api } from '@graasp/query-client';
+import { Context, ItemType, PackedItem, PermissionLevel } from '@graasp/sdk';
 
 import { createFileRoute } from '@tanstack/react-router';
 
-import { useAuth } from '@/AuthContext';
+import { type AuthenticatedMember, useAuth } from '@/AuthContext';
 import { NS } from '@/config/constants';
+import { API_HOST } from '@/config/env';
+import { axios } from '@/config/queryClient';
+import AppItem from '@/ui/items/AppItem';
 
-import AppContent from '~analytics/charts-layout/AppsContent';
 import SectionTitle from '~analytics/common/SectionTitle';
-import { APPS_ID } from '~analytics/config/selectors';
+import {
+  APPS_ID,
+  APP_ITEM_CLASS_NAME,
+  buildAppItemSelector,
+} from '~analytics/config/selectors';
 import { DataContext } from '~analytics/context/DataProvider';
 
 export const Route = createFileRoute('/analytics/items/$itemId/apps')({
@@ -30,9 +39,14 @@ function AppsAnalyticPage(): JSX.Element | null {
     return (
       <>
         <SectionTitle title={t('APPS_ANALYTICS_TITLE')} />
-        <Grid2 container spacing={2} p={2} id={APPS_ID}>
+        <Grid2 container spacing={2} p={2} id={APPS_ID} width="100%">
           {descendantApps.map((item) => (
-            <Grid2 key={item.id} size={{ xs: 12 }}>
+            <Grid2
+              key={item.id}
+              size={{ xs: 12 }}
+              id={buildAppItemSelector(item.id)}
+              className={APP_ITEM_CLASS_NAME}
+            >
               <AppContent item={item} member={user} />
             </Grid2>
           ))}
@@ -40,5 +54,48 @@ function AppsAnalyticPage(): JSX.Element | null {
       </>
     );
   }
+  return null;
+}
+
+function AppContent({
+  item,
+  member,
+}: Readonly<{
+  item: PackedItem;
+  member: AuthenticatedMember;
+}>): JSX.Element | null {
+  if (item.permission && item.type == ItemType.APP) {
+    const permission = item.permission;
+
+    return (
+      <>
+        <Typography variant="h6" align="center">
+          {item.name}
+        </Typography>
+        <Box sx={{ border: '1px solid rgba(0, 0, 0, 0.12)' }}>
+          <AppItem
+            isResizable={false}
+            item={item}
+            height="70vh"
+            requestApiAccessToken={(payload: {
+              id: string;
+              key: string;
+              origin: string;
+            }) => Api.requestApiAccessToken(payload, { API_HOST, axios })}
+            contextPayload={{
+              apiHost: API_HOST,
+              itemId: item.id,
+              accountId: member.id,
+              permission: permission || PermissionLevel.Read,
+              settings: item.settings,
+              lang: item.lang || member.lang,
+              context: Context.Analytics,
+            }}
+          />
+        </Box>
+      </>
+    );
+  }
+
   return null;
 }
