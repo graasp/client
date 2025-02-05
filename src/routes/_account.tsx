be6ed3d119b2e27e7577ec58000577ffff12ee57
spaenleh <spaenleh@gmail.com>
2025-02-05 11:37:40 +0100

    fix: access for guests is restricted

--- src/routes/_account.tsx
+++ src/routes/_account.tsx
@@ -1,13 +1,22 @@
 import { useTranslation } from 'react-i18next';
 
-import { Container, Stack, Typography } from '@mui/material';
+import { Button, Container, Stack, Typography } from '@mui/material';
 
 import { AccountType } from '@graasp/sdk';
 
-import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
+import {
+  Outlet,
+  createFileRoute,
+  redirect,
+  useCanGoBack,
+  useRouter,
+} from '@tanstack/react-router';
+import { LockIcon } from 'lucide-react';
 
+import { useAuth } from '@/AuthContext';
 import { NS } from '@/config/constants';
 import { LOG_IN_PAGE_PATH } from '@/config/paths';
+import { useButtonColor } from '@/ui/buttons/hooks';
 
 import { PageWrapper } from '~account/PageWrapper';
 
@@ -24,25 +33,51 @@ export const Route = createFileRoute('/_account')({
         },
       });
     }
-    if (auth.user.type !== AccountType.Individual) {
-      return <GuestNotAuthorized />;
-    }
   },
-  component: () => (
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { user } = useAuth();
+  if (user?.type !== AccountType.Individual) {
+    return <GuestNotAuthorized />;
+  }
+
+  return (
     <PageWrapper>
       <Container maxWidth="xl" sx={{ p: 2, height: '100%' }}>
         <Outlet />
       </Container>
     </PageWrapper>
-  ),
-});
+  );
+}
 
 function GuestNotAuthorized() {
   const { t } = useTranslation(NS.Account, { keyPrefix: 'GUEST_AUTH' });
+  const { t: translateCommon } = useTranslation(NS.Common);
+  const { color } = useButtonColor('primary');
+  const router = useRouter();
+  const canGoBack = useCanGoBack();
   return (
-    <Stack height="100vh">
-      <Typography variant="h1">{t('TITLE')}</Typography>
+    <Stack
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      p={5}
+      gap={5}
+    >
+      <Stack direction="row" alignItems="center" gap={5}>
+        <LockIcon size={80} color={color} />
+        <Typography variant="h1" color="primary">
+          {t('TITLE')}
+        </Typography>
+      </Stack>
       <Typography>{t('DESCRIPTION')}</Typography>
+      {canGoBack ? (
+        <Button onClick={() => router.history.back()}>
+          {translateCommon('BACK.BUTTON_TEXT')}
+        </Button>
+      ) : null}
     </Stack>
   );
 }
