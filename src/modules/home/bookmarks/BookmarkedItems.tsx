dcce98a5e34c1f283812124cc13b1842865b6779
spaenleh <spaenleh@gmail.com>
2025-02-13 10:34:51 +0100

    fix: add limit on bookmarks shown

--- src/modules/home/bookmarks/BookmarkedItems.tsx
+++ src/modules/home/bookmarks/BookmarkedItems.tsx
@@ -1,9 +1,18 @@
-import { type JSX, type ReactNode } from 'react';
+import { type JSX, type ReactNode, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import { Alert, Grid2 as Grid, Skeleton } from '@mui/material';
+import {
+  Alert,
+  Button,
+  Grid2 as Grid,
+  Skeleton,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
 import { v4 } from 'uuid';
 
+import { NS } from '@/config/constants';
 import { BOOKMARKED_ITEMS_ID } from '@/config/selectors';
 import { useBookmarkedItems } from '@/query/hooks/itemBookmark';
 
@@ -16,25 +25,57 @@ const GridWrapper = ({ children }: { children: ReactNode }): JSX.Element => (
 // mock data for placeholder skeleton
 const placeholderItems = Array(6).map(() => ({ id: v4() }));
 
+const useResponsiveMaxItems = () => {
+  const theme = useTheme();
+  const largerThanXl = useMediaQuery(theme.breakpoints.up('xl'));
+  const largerThanMd = useMediaQuery(theme.breakpoints.up('md'));
+  const largerThanSm = useMediaQuery(theme.breakpoints.up('sm'));
+
+  if (largerThanXl) {
+    return 24;
+  }
+  if (largerThanMd) {
+    return 12;
+  }
+  if (largerThanSm) {
+    return 6;
+  }
+  return 4;
+};
+
 export function BookmarkedItems() {
+  const { t } = useTranslation(NS.Home, { keyPrefix: 'BOOKMARKED_ITEMS' });
   const { data: bookmarkedItems, isPending, isError } = useBookmarkedItems();
+  const [showAll, setShowAll] = useState(false);
+
+  const maxItems = useResponsiveMaxItems();
 
   if (bookmarkedItems) {
+    const shownBookmarks = showAll
+      ? bookmarkedItems
+      : bookmarkedItems.slice(0, maxItems);
     return (
-      <Grid
-        id={BOOKMARKED_ITEMS_ID}
-        width="100%"
-        container
-        // needs to be "spacing" because with gap it does not fill the whole line
-        spacing={2}
-        justifyItems="center"
-      >
-        {bookmarkedItems.map(({ item }) => (
-          <GridWrapper key={item.id}>
-            <ItemCard key={item.id} item={item} />
-          </GridWrapper>
-        ))}
-      </Grid>
+      <>
+        <Grid
+          id={BOOKMARKED_ITEMS_ID}
+          width="100%"
+          container
+          // needs to be "spacing" because with gap it does not fill the whole line
+          spacing={2}
+          justifyItems="center"
+        >
+          {shownBookmarks.map(({ item }) => (
+            <GridWrapper key={item.id}>
+              <ItemCard key={item.id} item={item} />
+            </GridWrapper>
+          ))}
+        </Grid>
+        {maxItems < bookmarkedItems.length ? (
+          <Button onClick={() => setShowAll((s) => !s)}>
+            {showAll ? t('SHOW_LESS') : t('SHOW_MORE')}
+          </Button>
+        ) : null}
+      </>
     );
   }
 
