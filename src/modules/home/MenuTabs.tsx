0573eea5e9bbccedcde050d874ac4d320f921ed9
Basile Spaenlehauer <spaenleh@gmail.com>
2025-02-14 11:22:20 +0100

    fix: refactor menuTabs to use tsr link (#919)

--- src/modules/home/MenuTabs.tsx
+++ src/modules/home/MenuTabs.tsx
@@ -2,16 +2,12 @@ import { useTranslation } from 'react-i18next';
 
 import { Stack } from '@mui/material';
 
-import { useRouterState } from '@tanstack/react-router';
 import { HomeIcon, LibraryBigIcon, TrashIcon } from 'lucide-react';
 
 import { NS } from '@/config/constants';
 import { MenuTab } from '@/ui/MenuTab/MenuTab';
 
 export function MenuTabs() {
-  const {
-    location: { pathname },
-  } = useRouterState();
   const { t } = useTranslation(NS.Home, { keyPrefix: 'MENU' });
 
   return (
@@ -22,23 +18,16 @@ export function MenuTabs() {
       justifyContent="flex-end"
       width="100%"
     >
-      <MenuTab
-        icon={<HomeIcon fontSize="" />}
-        title={t('HOME')}
-        to="/home"
-        active={pathname === '/home'}
-      />
+      <MenuTab icon={<HomeIcon />} title={t('HOME')} to="/home" />
       <MenuTab
         icon={<LibraryBigIcon />}
         title={t('PUBLISHED_ITEMS')}
         to="/builder/published"
-        active={pathname === '/builder/published'}
       />
       <MenuTab
         icon={<TrashIcon />}
         title={t('RECYCLED_ITEMS')}
         to="/builder/recycled"
-        active={pathname === '/builder/recycled'}
       />
     </Stack>
   );
