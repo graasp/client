e8953677653489c254bb87739ab6d4e60c5a4caf
Basile Spaenlehauer <spaenleh@gmail.com>
2025-05-09 09:09:01 +0200

    fix: add the contact information to the maintenance banner (#996)
    * fix: add the contact information to the maintenance banner
    
    * fix: suggestions for text
    
    Co-authored-by: Kim Lan Phan Hoang <klphanh@gmail.com>
    
    ---------
    
    Co-authored-by: Kim Lan Phan Hoang <klphanh@gmail.com>

--- src/routes/_memberOnly/_homeLayout/home.tsx
+++ src/routes/_memberOnly/_homeLayout/home.tsx
@@ -2,7 +2,7 @@ import { Divider, Stack, useMediaQuery, useTheme } from '@mui/material';
 
 import { createFileRoute } from '@tanstack/react-router';
 
-import MaintenanceAnnouncement from '@/modules/home/MaintenanceAnnouncement';
+import { MaintenanceAnnouncement } from '@/modules/home/MaintenanceAnnouncement';
 import { BookmarkedItems } from '@/modules/home/bookmarks/BookmarkedItems';
 
 import { FilterItemsContextProvider } from '~builder/components/context/FilterItemsContext';
