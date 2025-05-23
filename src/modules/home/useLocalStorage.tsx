42c838e92fd79dffa9db42d0f119527df2dd16d3
spaenleh <spaenleh@gmail.com>
2025-05-23 14:56:06 +0200

    chore: add eslint ignores

--- src/modules/home/useLocalStorage.tsx
+++ src/modules/home/useLocalStorage.tsx
@@ -4,14 +4,20 @@ export function useLocalStorage<T>(key: string, defaultValue?: T) {
   const [value, setValue] = useState<T | undefined>();
 
   // need this effect to correctly change value when the key changes
-  useEffect(() => {
-    const storedValue = localStorage.getItem(key);
-    if (storedValue) {
-      setValue(JSON.parse(storedValue));
-    } else {
-      setValue(defaultValue);
-    }
-  }, [key]);
+  useEffect(
+    () => {
+      const storedValue = localStorage.getItem(key);
+      if (storedValue) {
+        // eslint-disable-next-line @eslint-react/hooks-extra/no-direct-set-state-in-use-effect
+        setValue(JSON.parse(storedValue));
+      } else {
+        // eslint-disable-next-line @eslint-react/hooks-extra/no-direct-set-state-in-use-effect
+        setValue(defaultValue);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [key],
+  );
 
   const changeValue = (v: T) => {
     localStorage.setItem(key, JSON.stringify(v));
