7e5b588625624f8d2691f942136bbdf7030e06b5
kim <kim.phanhoang@epfl.ch>
2025-07-25 16:46:03 +0200

    refactor: fix tests

--- cypress/e2e/analytics/exportAnalytics.cy.ts
+++ cypress/e2e/analytics/exportAnalytics.cy.ts
@@ -1,10 +1,15 @@
-import { ExportActionsFormatting, MemberFactory } from '@graasp/sdk';
+import {
+  ExportActionsFormatting,
+  HttpMethod,
+  MemberFactory,
+} from '@graasp/sdk';
 
 import {
   EXPORT_ACTIONS_BUTTON_ID,
   buildSelectExportFormatID,
 } from '../../../src/modules/analytics/config/selectors';
 import { ITEM_TO_EXPORT } from '../../fixtures/analytics/items';
+import { API_HOST } from '../../support/env';
 
 describe('Check exporting analytics for allowed formats', () => {
   beforeEach(() => {
@@ -14,6 +19,16 @@ describe('Check exporting analytics for allowed formats', () => {
   [ExportActionsFormatting.CSV, ExportActionsFormatting.JSON].forEach(
     (format) => {
       it(`Check export to ${format} button disabled after first export`, () => {
+        cy.intercept(
+          {
+            method: HttpMethod.Post,
+            url: `${API_HOST}/items/${ITEM_TO_EXPORT.id}/actions/export?format=${format}`,
+          },
+          ({ reply }) => {
+            return reply({});
+          },
+        ).as('export');
+
         cy.visit(`/analytics/items/${ITEM_TO_EXPORT.id}/export`);
         cy.get(`#${buildSelectExportFormatID(format)}`).click({
           force: true,
