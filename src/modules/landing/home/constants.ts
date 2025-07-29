3f771c74bb969824cc7b97ed05fb80ab3c6c7a72
kim <kim.phanhoang@epfl.ch>
2025-07-29 17:16:34 +0200

    feat: show capsule examples on landing page

--- src/modules/landing/home/constants.ts
+++ src/modules/landing/home/constants.ts
@@ -1,2 +1,69 @@
 export const TEACHER_USER_STORY = 'teacher-story';
 export const RESEARCHER_USER_STORY = 'researcher-story';
+
+export const EN_CAPSULES = [
+  {
+    title: 'Understanding the climatic greenhouse effect',
+    imageSrc: '/capsules/climate.webp',
+    description:
+      'Discover the physical mechanisms behind our climate and its warming.',
+    url: 'https://graasp.org/player/86281673-76cf-4af0-a09d-86c287ed0e6b/86281673-76cf-4af0-a09d-86c287ed0e6b?fullscreen=false',
+  },
+  {
+    title: 'ESLint Lesson',
+    description:
+      'Grasp the fundamental Newtonian mechanics concepts with interactive simulations.',
+    imageSrc: '/capsules/eslint.jpeg',
+    url: 'https://graasp.org/player/0e56d2fb-4344-4be5-95e6-531548158b14/0e56d2fb-4344-4be5-95e6-531548158b14?fullscreen:false',
+  },
+
+  {
+    title: "The Hitchhiker's Guide to OERs",
+    description: 'Using Graasp for designing Open Educational Resources.',
+    imageSrc: '/capsules/hitchhiker_oer.webp',
+    url: 'https://graasp.org/player/e7cf4d11-f830-47a7-b281-b81f59726c49/e7cf4d11-f830-47a7-b281-b81f59726c49?fullscreen:false',
+  },
+  {
+    title: 'Newtonian mechanics',
+    description:
+      'Grasp the fundamental Newtonian mechanics concepts with interactive simulations.',
+    imageSrc: '/capsules/newtonian_mechanics.png',
+    url: 'https://graasp.org/player/757861bf-944b-42f3-8f89-a1fa10cad61b/757861bf-944b-42f3-8f89-a1fa10cad61b?fullscreen=false',
+  },
+  {
+    title: 'Towards a global climate model',
+    imageSrc: '/capsules/globaleffect.webp',
+    description: "Exploration of a model of the planet's climate.",
+    url: 'https://graasp.org/player/5fc87751-e6e7-4aaf-9611-7571e9e37f8c/5fc87751-e6e7-4aaf-9611-7571e9e37f8c?fullscreen:false',
+  },
+  {
+    title: 'Sustainable Development Goals (SDGs)',
+    imageSrc: '/capsules/sdg.jpeg',
+    description:
+      'This learning capsule presents the Sustainable Development Goals (SDGs).',
+    url: 'https://graasp.org/player/08a475c9-ed96-41d5-a6ce-6aa6170fc4cd/08a475c9-ed96-41d5-a6ce-6aa6170fc4cd?fullscreen=false',
+  },
+];
+
+export const FR_CAPSULES = [
+  {
+    title: 'Préparation à la dissection du coeur',
+    imageSrc: '/capsules/coeur.jpg',
+    description:
+      'Cette capsule va te permettre de te préparer à la dissection du cœur.',
+    url: 'https://graasp.org/player/0604830c-6241-4fc0-8337-c64390d8b2c3/fbc0e22d-c972-461e-bf40-ec83d3ce4e9f?fullscreen:false',
+  },
+  {
+    title: "GLOBE : utiliser des mesures prises dans l'environnement",
+    imageSrc: '/capsules/Globe_logo.png',
+    description: "Dispositif d'apprentissage autonome pour les élèves",
+    url: 'https://graasp.org/player/95b4e981-56d0-42b2-9672-13c546c79aa1/95b4e981-56d0-42b2-9672-13c546c79aa1?fullscreen:false',
+  },
+  {
+    title: 'Virus SARS-CoV-2',
+    imageSrc: '/capsules/covid.jpeg',
+    description:
+      "Observation et représentation de l'agent infectieux du COVID 19",
+    url: 'https://graasp.org/player/8bea12d8-061d-4c00-807d-6501e2778312/8bea12d8-061d-4c00-807d-6501e2778312?fullscreen=false',
+  },
+];
