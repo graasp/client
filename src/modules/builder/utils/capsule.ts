3144db5e55516ac1cf97af2db1f77a99ba45edde
kim <kim.phanhoang@epfl.ch>
2026-02-02 09:59:04 +0100

    refactor: update comment

--- src/modules/builder/utils/capsule.ts
+++ src/modules/builder/utils/capsule.ts
@@ -3,7 +3,7 @@ import { DiscriminatedItem, ItemType, isCapsule } from '@graasp/sdk';
 /**
  * Utility function to get the item's type, including capsule
  * @param item
- * @returns
+ * @returns type of the item
  */
 export function getItemType(item: DiscriminatedItem) {
   if (item.type === ItemType.FOLDER && isCapsule(item)) {
