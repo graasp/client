c73b23e876e9c24066c9dde7857eeef688ce0e12
Santiago Rojas <126520289+Santirv17@users.noreply.github.com>
2026-06-29 11:05:43 +0200

    fix: apply prettier style

--- src/modules/player/rightPanel/SideContent.tsx
+++ src/modules/player/rightPanel/SideContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, type JSX } from 'react';
+import { type JSX, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { IconButton, Stack, Tooltip, styled } from '@mui/material';
